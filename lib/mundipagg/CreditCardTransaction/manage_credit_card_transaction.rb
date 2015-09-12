983043c2a1cf68e4628669dba5f0b3b15c3dafcc
Munir Wanis <fallen.angel.pp@gmail.com>
2015-09-11 21:57:26 -0300

    Calcel Request
    Adicionado método de cancelamento

--- lib/mundipagg/CreditCardTransaction/manage_credit_card_transaction.rb
+++ lib/mundipagg/CreditCardTransaction/manage_credit_card_transaction.rb
@@ -3,4 +3,10 @@ class ManageCreditCardTransaction
   attr_accessor :AmountInCents
   attr_accessor :TransactionKey
   attr_accessor :TransactionReference
+
+  def to_json
+    hash = {}
+    instance_variables.each {|var| hash[var.to_s.delete('@')] = instance_variable_get(var) }
+    hash
+  end
 end
